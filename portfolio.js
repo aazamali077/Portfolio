9e72168bed65e33a63430922e0bae1b2c5666104
Aazam <abuzarali1098@gmail.com>
2024-09-30 08:07:16 +0530

    Added dark and light mode functionality

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+var icon = document.getElementById("dark-mode");
+
+icon.onclick = function(){
+    document.body.classList.toggle("mode-change");
+    if(document.body.classList.contains("mode-change")){
+        icon.src = "mode images/sun.png";
+    }
+    else{
+        icon.src = "mode images/moon.png";
+    }
+}
